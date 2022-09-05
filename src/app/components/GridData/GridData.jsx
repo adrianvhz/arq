266a6971064f368ab223b80a0b7c29eadc991cda
Juan Carlos Castillo <juan@accounttech.com>
2022-09-05 01:33:13 -0500

    responsive

--- src/app/components/GridData/GridData.jsx
+++ src/app/components/GridData/GridData.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import moment from 'moment';
 import Grid from '@mui/material/Grid';
 import { RowList } from './RowList';
+import { Box } from '@mui/material';
 
 const styleGrid = {
     border: "1.5px solid #AFAFAF",
@@ -13,19 +14,13 @@ const styleGrid = {
 const containerCustom = {
     marginLeft: "auto",
     marginRight: "auto",
-    width: "1000px",
-    // overflowX: "scroll",
+    width: "90%",
+}
 
-    '@media (max-width: 1024px)': {
-        width: '85%',
-        height: '100%',
-        // top: '25%',
-
-        // transform: 'translate(0, 0)',
-        // borderRadius: '0',
-    }
-
-
+const dataBoxGrid = {
+    width: "1050px",
+    marginLeft: "auto",
+    marginRight: "auto",
 
 }
 const GridData = (data) => {
@@ -34,12 +29,6 @@ const GridData = (data) => {
     const [dataFiltrada, setDataFiltrada] = useState([])
     const [fechaInicio, setFechaInicio] = useState('')
     const [fechaFin, setFechaFin] = useState('')
-
-    console.log(dataFiltrada, "dataFiltrada")
-
-    // useEffect(() => {
-    //     setDataFiltrada(data.data)
-    // }, [])
 
     const options = (e) => {
         switch (e) {
@@ -95,10 +84,7 @@ const GridData = (data) => {
             dataFilterDateRange()
             return
         }
-        // if (fechaInicio === 'Invalid date' || fechaFin === 'Invalid date') {
-        //     setDataFiltrada(data.data)
-        //     return
-        // }
+
         if (!fechaInicio || !fechaFin || fechaInicio === 'Invalid date' || fechaFin === 'Invalid date') {
             setDataFiltrada(data.data)
             return
@@ -113,11 +99,11 @@ const GridData = (data) => {
         <div style={containerCustom} >
 
             <Grid container spacing={3} sx={{ marginBottom: "1.75rem" }}>
-                <Grid item xs={4}>
+                <Grid item xs={12} sm={6} lg={4}>
                     <label>Buscar</label>
                     <input type="text" placeholder="Escribe aquí" style={{ width: "100%" }} onChange={(e) => dataFilter(e.target.value)} />
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={12} sm={6} lg={3}>
                     <label>Desde</label>
                     <input type="date" className="" style={{ width: "100%" }}
                         // onChange={(e) => dataFilterDate(moment(e.target.value).format('DD/MM/YYYY'))} 
@@ -126,14 +112,14 @@ const GridData = (data) => {
                     />
 
                 </Grid>
-                <Grid item xs={3}>
+                <Grid xs={12} item sm={6} lg={3}>
                     <label>Hasta</label>
                     <input type="date" className="" style={{ width: "100%" }}
                         onChange={(e) => setFechaFin(moment(e.target.value).format('DD/MM/YYYY'))}
 
                     />
                 </Grid>
-                <Grid item xs={2}>
+                <Grid item xs={12} sm={6} lg={2}>
                     <label>Mostrar</label>
                     <select style={{ width: "100%" }} onChange={(e) => options(e.target.value)}>
                         <option value="0">Todos</option>
@@ -143,46 +129,56 @@ const GridData = (data) => {
                     </select>
                 </Grid>
             </Grid>
-            <Grid container>
-                <Grid item xs={0.5} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    {/* <Box width="100%" sx={styleGrid}> */}
-                    ID
+            <Box sx={{
+                width: "100%",
+                overflowX: "auto",
+            }}>
 
-                    {/* </Box> */}
-                </Grid>
-                <Grid item xs={0.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+                <div style={dataBoxGrid}>
 
-                    Tipo
-                </Grid>
-                <Grid item xs={2} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Nombre del proyecto
-                </Grid>
-                <Grid item xs={1} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Ubicacion
-                </Grid>
-                <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Responsable
-                </Grid>
-                <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Fecha de Elab.
-                </Grid>
-                <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Fecha de Act.
-                </Grid>
-                <Grid item xs={2} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Nombe del cliente
-                </Grid>
-                <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
-                    Acciones
-                </Grid>
+                    <Grid container>
+                        <Grid item xs={0.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            {/* <Box width="100%" sx={styleGrid}> */}
+                            ID
+
+                            {/* </Box> */}
+                        </Grid>
+                        <Grid item xs={0.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+
+                            Tipo
+                        </Grid>
+                        <Grid item xs={2} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Nombre del proyecto
+                        </Grid>
+                        <Grid item xs={1} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Ubicacion
+                        </Grid>
+                        <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Responsable
+                        </Grid>
+                        <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Fecha de Elab.
+                        </Grid>
+                        <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Fecha de Act.
+                        </Grid>
+                        <Grid item xs={2} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Nombe del cliente
+                        </Grid>
+                        <Grid item xs={1.5} sx={{ ...styleGrid, fontWeight: 600 }}>
+                            Acciones
+                        </Grid>
 
 
 
 
-            </Grid>
-            {dataFiltrada.map((row, index) =>
-                <RowList key={index} row={row} index={index} data={dataFiltrada} />
-            )}
+                    </Grid>
+                    {dataFiltrada.map((row, index) =>
+                        <RowList key={index} row={row} index={index} data={dataFiltrada} />
+                    )}
+                </div>
+            </Box>
+
 
 
 
